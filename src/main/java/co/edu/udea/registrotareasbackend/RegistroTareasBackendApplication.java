55cb83c5b05d6c29096072b2b85b1d490b2ce544
juan david morales muñoz <5810910-juangenoma@users.noreply.gitlab.com>
2021-08-14 16:41:36 -0500

    primer commit

---
+++ src/main/java/co/edu/udea/registrotareasbackend/RegistroTareasBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.registrotareasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroTareasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroTareasBackendApplication.class, args);
+    }
+
+}
