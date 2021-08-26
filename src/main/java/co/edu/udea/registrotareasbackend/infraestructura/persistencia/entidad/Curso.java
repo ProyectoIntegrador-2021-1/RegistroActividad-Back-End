5adc2049c67933c7dd53db66f70f9182eeb41fff
juan david morales muñoz <5810910-juangenoma@users.noreply.gitlab.com>
2021-08-25 21:38:02 -0500

    entidad

---
+++ src/main/java/co/edu/udea/registrotareasbackend/infraestructura/persistencia/entidad/Curso.java
@@ -0,0 +1,25 @@
+package co.edu.udea.registrotareasbackend.infraestructura.persistencia.entidad;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "curso")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "estado")
+    private Boolean estado;
+}
